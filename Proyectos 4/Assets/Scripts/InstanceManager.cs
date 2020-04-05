6df2687dbf4ec4dd1c07efbd476357f6b3bd0af4
Samuel Maqueda <samuel.maqueda@hotmail.com>
2020-04-05 18:10:28 +0100

    scripts

---
+++ Proyectos 4/Assets/Scripts/InstanceManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceManager : MonoBehaviour
+{
+	public GameObject prefabTorreta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("pulsado");
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit infoImpacto;
+			if (Physics.Raycast(ray, out infoImpacto))
+			{
+				Debug.Log("impactado");
+				Vector3 puntoImpacto = infoImpacto.point;
+				Instantiate(prefabTorreta, puntoImpacto, Quaternion.identity);
+			}
+		}
+	}
+}
---
+++ Proyectos 4/Assets/Scripts/Torreta.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torreta : MonoBehaviour
+{
+	public GameObject prefabBala;
+	public Transform cañon;
+	GameObject intruso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("entro");
+		var pablosito = other.gameObject;
+		intruso = pablosito;
+		transform.LookAt(intruso.transform);
+		Instantiate(prefabBala, cañon.position, Quaternion.identity);
+	}
+}
